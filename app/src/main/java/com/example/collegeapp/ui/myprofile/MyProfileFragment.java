e73d11ee19a01d233167c8a0374ea9a410bc3374
vladi <vladimir.gorgieski@hotmail.com>
2020-09-26 19:07:17 +0200

    added navigation

---
+++ app/src/main/java/com/example/collegeapp/ui/myprofile/MyProfileFragment.java
@@ -0,0 +1,35 @@
+package com.example.collegeapp.ui.myprofile;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.collegeapp.R;
+
+public class MyProfileFragment extends Fragment {
+    private MyProfileViewModel myProfileViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        myProfileViewModel =
+                ViewModelProviders.of(this).get(MyProfileViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_myprofile, container, false);
+        final TextView textView = root.findViewById(R.id.text_myprofile);
+        myProfileViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+
+}
